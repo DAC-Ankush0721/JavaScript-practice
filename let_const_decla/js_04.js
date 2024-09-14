b73a56dc0a919d708be00c6a666341ca9a1b7d55
DAC-Ankush0721 <ankushkhandare61@gmail.com>
2024-09-14 18:53:56 +0530

    epi04

---
+++ let_const_decla/js_04.js
@@ -0,0 +1,17 @@
+  
+
+//     const a = 10;
+//     a = 20;
+//    console.log(a);  // we cannot change the value of variale in const 
+
+
+   let b = 100;
+   b =  200;
+   console.log(b);   // but in case of let we can change the value and modify it
+   
+
+
+   var x = 20;
+   x =40;
+   console.log(x);
+   
