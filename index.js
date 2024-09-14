d1db18f20ab83a4bc4283f505d4e77b30ad6252f
DAC-Ankush0721 <ankushkhandare61@gmail.com>
2024-09-14 15:04:55 +0530

    ep-01

---
+++ index.js
@@ -0,0 +1,21 @@
+// template literals 
+
+const age =  20;
+const fullname = "john Doe ";
+
+//output :Hello Mr John Doe your age is : 28
+
+console.log("Hello Mr" + fullname + "your age is :" + age);
+
+
+console.log(`Hello Mr ${fullname} your age is : ${age}`); //by using template literals 
+
+
+//q print sum of two numberrs 
+
+const a = 10;
+const b =  20;
+ 
+//by using template liteerals
+
+console.log(`Sum of ${a} and ${b} is : ${a+b}`);
