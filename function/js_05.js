843d46f12706f34bea1613693e8282dd8f430c41
DAC-Ankush0721 <ankushkhandare61@gmail.com>
2024-09-14 19:51:38 +0530

    epi06

--- function/js_05.js
+++ function/js_05.js
@@ -14,12 +14,12 @@
 // sumOfTwoNumber(1,2);
 
 
-/// calback function
 
 
 
+    /// calback function                              
 
-const doSomething =function (calback){
+const doSomething =function (calback){ //argument recive 
     console.log("samose ban rahe hai...");
     setTimeout(function(){
 
@@ -33,4 +33,73 @@ const callback =  function (){
     
 }
 
-doSomething(callback);
+doSomething(callback); // argument pass  
+
+
+// Higher order function
+
+// Higher order functions
+
+const radius = [1,2,3,4];
+
+const area = function(radiusArray){
+    const result = [];
+    for(let i = 0; i < radiusArray.length; i++){
+        const a = Math.PI * radiusArray[i] * radiusArray[i];
+        result.push(a);
+    }
+    return result;
+}
+
+const ans = area(radius);
+console.log(ans);
+
+const circumference = function(radiusArray){
+    const result = [];
+    for(let i = 0; i < radiusArray.length; i++){
+        const a = 2 * Math.PI * radiusArray[i];
+        result.push(a);
+    }
+    return result;
+}
+
+const ans2 = circumference(radius);
+console.log(ans2);
+
+
+// 
+
+const formulaOfArea = function(r){
+    return Math.PI * r * r;
+}
+
+const formulaOfCircumference = function(r){
+    return 2 * Math.PI * r;
+}
+
+
+const calculate = function(radiusArray, formula){
+    const result = [];
+    for(let i = 0; i < radiusArray.length; i++){
+        const a = formula(radiusArray[i]);
+        result.push(a);
+    }
+    return result;
+}
+const ans3 = calculate(radius, formulaOfArea);
+const ans4 = calculate(radius, formulaOfCircumference);
+console.log(ans3);
+console.log(ans4);
+
+// Arrow function 
+
+const multiply = (num1, num2) => {
+   return num1*num2;
+}
+console.log(multiply(2,3));
+
+
+// import  name  from "../ep-07";
+// console.log(name);
+
+ 
